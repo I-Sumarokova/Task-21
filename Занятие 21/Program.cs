6982e90c28406b8b71b13faeef85d0cea66d1e76
asus <asus@LAPTOP-S46F7906>
2022-01-03 11:23:59 +0300

    Добавьте файлы проекта.

---
+++ Занятие 21/Program.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace Занятие_21
+{
+    class Program
+    {
+        public static int n;
+        public static int m;
+        public static int[,] array;
+        public static void sadovnik1()
+        {
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < m; j++)
+                {
+                    if (array[i, j] == 0)
+                    {
+                        array[i, j] = 1;
+                        Thread.Sleep(1);
+                    }
+                }
+            }
+        }
+        public static void sadovnik2()
+        {
+            for (int i = m-1; i > 0; i--)
+            {
+                for (int j = n-1; j > 0; j--)
+                {
+                    if (array[j, i] == 0)
+                    {
+                        array[j, i] = 2;
+                        Thread.Sleep(1);
+                    }
+                }
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите размерность массива");
+            n = Convert.ToInt32(Console.ReadLine());
+            m = Convert.ToInt32(Console.ReadLine());
+            array = new int[n, m];
+
+            Thread myThread = new Thread(sadovnik1);
+            Thread myThread1 = new Thread(sadovnik2);
+
+            myThread.Start();
+            myThread1.Start();
+
+            myThread.Join();
+            myThread1.Join();
+
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < m; j++)
+                {
+                    Console.Write(array[i,j]+" ");
+                }
+                Console.WriteLine();
+            }
+            Console.ReadLine();
+        }
+    }
+}
